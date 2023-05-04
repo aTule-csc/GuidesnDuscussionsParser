1f7bf11f3a2c4c221e69a31b82830cb8ad267f55
aT <krivonocovf@gmail.com>
2023-05-04 10:48:08 +0500

    add def steam_parser

--- ParserTest.py
+++ ParserTest.py
@@ -8,13 +8,26 @@ import os
 load_dotenv()
 import json
 bot = telebot.TeleBot(os.getenv("token"))
+def steam_parse():
+    html = requests.get("https://steamcommunity.com/app/218620/discussions/?l=russian").text
+    soup = BeautifulSoup(html, 'html.parser') 
+    divs = soup.find_all("div", class_ = 'forum_topic')
+    posts = []
+    for post in divs:
+        url = post.find("a", class_ = "forum_topic_overlay").get("href")
+        name = post.find("div", class_ = "forum_topic_name").text.strip() 
+        posts.append((name, url))
+    return posts
 
+
+print(steam_parse())
+
+"""
 a=requests.get('https://steamcommunity.com/app/218620/discussions/').text
 print(a)
-"""soup = BeautifulSoup(a, "html.parser")
+soup = BeautifulSoup(a, "html.parser")
 info = soup.find_all('div',class_='forum_topic  unread')
 print(a)
-""""""
 req = urllib.request.Request('https://steamcommunity.com/app/218620/discussions/',)
 with urllib.request.urlopen(req) as response:
     the_page = response.read()
