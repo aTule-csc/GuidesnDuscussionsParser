31146679c1a34a01b7131e9e2ac24dd9423a372a
aT <krivonocovf@gmail.com>
2023-06-04 16:05:24 +0500

    add def diss_sort

--- ParserTest.py
+++ ParserTest.py
@@ -10,7 +10,7 @@ load_dotenv()
 import json
 #bot = telebot.TeleBot(os.getenv("token"))
 def steam_parse():
-    html = requests.get("https://steamcommunity.com/app/218620/discussions/?l=russian").text
+    html = requests.get("https://steamcommunity.com/app/218620/discussions/?l=russian&p=1").text
     soup = BeautifulSoup(html, 'html.parser') 
     divs = soup.find_all("div", class_ = 'forum_topic')
     posts = []
@@ -27,17 +27,36 @@ def steam_parse():
         lastpost = post.find("div", class_ = "forum_topic_lastpost").get("title")
         posts.append((name, url,author,reply_count,lastpost))
     return posts
-a = 'Jacket'
-b = steam_parse()
-#def sort(a,b):
-results = []
-for i in b:
-    pattern =re.compile(a)
-    line = i[0]
-    c=bool(pattern.search(line))
-    if c == True:
-        results.append(i)
-print(results)
+def diss_sort_test():
+    a = 'Jacket'
+    b = steam_parse()
+    #def sort(a,b):
+    results = []
+    for i in b:
+        pattern =re.compile(a)
+        line = i[0]
+        c=bool(pattern.search(line))
+        if c == True:
+            results.append(i)
+    print(results)
+
+def guides_parser():
+    html=requests.get("https://steamcommunity.com/app/218620/guides/?searchText=&browsefilter=trend&browsesort=creationorder&requiredtags%5B0%5D=-1&p=1").text
+    soup = BeautifulSoup(html, 'html.parser')
+    find= soup.find_all("a", class_ = "workshopItemCollection ugc_show_warning_image ugc")
+    
+    guides = []
+    for guide in find:
+        url=guide.get("href")
+        title=guide.find("div", class_ ="workshopItemTitle").text.strip()
+        author = guide.find("div", class_ = "workshopItemAuthorLine")
+        author = author.find('span').text.strip()
+        desc = guide.find("div",class_ = "workshopItemShortDesc").text.strip()
+        guides.append((title, author, url, desc))
+    
+    return guides
+
+print(guides_parser())
 
 
     
