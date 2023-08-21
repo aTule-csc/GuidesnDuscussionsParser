dfc0a1699bc4d7f633fb470f92907433615d7a29
aTule-csc <113103385+aTule-csc@users.noreply.github.com>
2023-08-21 22:38:55 +0500

    add def game_list (shell only)

--- Guides&DuscussionsParser.py
+++ Guides&DuscussionsParser.py
@@ -64,6 +64,8 @@ def list_change(message):
         markup.add(item2)
         bot.send_message(message.chat.id,"Как именно изменить список?",reply_markup=markup)
     set_game_id(message)
+    game_list(message)
+
 
 def disc_parser_wof(message):
     if message.text=="Первая страница обсуждений":
@@ -88,8 +90,35 @@ def game_add(message):
         con = sqlite3.connect("users_games.db")
         cursor = con.cursor()
         cursor.execute(f"UPDATE Users_games SET user_game = {game_id} WHERE user_id = {message.from_user.id}")
+        con.commit()
+        cursor.close()
+        bot.send_message(message.from_user.id, "Замена произведена")
 
-
+def game_list(message):
+    if message.text=="Выбрать игру из списка":
+        markup = types.InlineKeyboardMarkup()
+        item1 = types.InlineKeyboardButton('1.Apex Legends', callback_data='qs_csc')
+        item2 = types.InlineKeyboardButton('2.Control Ultimate Edition', callback_data='qs_shosh')
+        item3 = types.InlineKeyboardButton('3.Dead by Daylight', callback_data='qs_csc')
+        item4 = types.InlineKeyboardButton('4.Deep Rock Galactic', callback_data='qs_csc')
+        item5 = types.InlineKeyboardButton('5.Dishonored RHCP', callback_data='qs_csc')
+        item6 = types.InlineKeyboardButton('6.Dishonored 2', callback_data='qs_csc')
+        item7 = types.InlineKeyboardButton('7.DOOM Eternal', callback_data='qs_csc')
+        item8 = types.InlineKeyboardButton('8.Dying Light', callback_data='qs_csc')
+        item9 = types.InlineKeyboardButton('9.Dying Light 2', callback_data='qs_csc')
+        item10 = types.InlineKeyboardButton('10.Half Life', callback_data='qs_csc')
+        item11 = types.InlineKeyboardButton('11.Half Life 2', callback_data='qs_csc')
+        item12 = types.InlineKeyboardButton('12.Killing Floor 2', callback_data='qs_csc')
+        item13 = types.InlineKeyboardButton("13.Mirror's Edge", callback_data='qs_csc')
+        item14 = types.InlineKeyboardButton("14.Mirror's Edge™ Catalyst", callback_data='qs_csc')
+        item15 = types.InlineKeyboardButton('15.Payday 2', callback_data='qs_csc')
+        item16 = types.InlineKeyboardButton('16.Portal 2', callback_data='qs_csc')
+        item17 = types.InlineKeyboardButton('17.Terraria', callback_data='qs_csc')
+        item18 = types.InlineKeyboardButton('18.Team Fortress 2', callback_data='qs_csc')
+        item19 = types.InlineKeyboardButton('19.Titanfall 2', callback_data='qs_csc')
+        item20 = types.InlineKeyboardButton('20.Warframe', callback_data='qs_csc')
+        markup.add([item1,item2],item3,item4,item5,item6,item7,item8,item9,item10,item11,item12,item13,item14,item15,item16,item17,item18,item19,item20)
+        bot.send_message(message.chat.id,"Test",reply_markup=markup)
 bot.infinity_polling()
 
 
